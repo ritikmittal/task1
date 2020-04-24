19715345b763ebaee6649d27258a753c08a916a9
Ritik Mittal <55192608+ritikmittal@users.noreply.github.com>
2020-04-24 22:17:50 +0530

    Update setup.sh

--- task1/setup.sh
+++ task1/setup.sh
@@ -5,7 +5,7 @@ if [[ $1 =~ task1 ]] && [[ -d ${1} ]]; then
 	echo "alias permit='sudo ${1}/scripts/permit.sh'" >> ~/.bashrc
 	echo "alias autoSchedule='${1}/scripts/autoSchedule.sh'" >> ~/.bashrc
 	echo "alias attendance='${1}/scripts/attendance.sh'" >> ~/.bashrc
-	chmod 775 ${1}/setup2 ${1}/scripts/*
+	chmod 775 ${1}/setup2.sh ${1}/scripts/*
 else echo "please give proper path in format of /home/user/....../task1"
 fi
 
